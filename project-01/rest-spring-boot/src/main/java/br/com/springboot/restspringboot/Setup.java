3e20ea09ebea0584df6fa48a357536b83f66de80
Santos <tuany.a.santos@accenture.com>
2023-10-17 00:25:51 -0300

    initial commit

---
+++ project-01/rest-spring-boot/src/main/java/br/com/springboot/restspringboot/Setup.java
@@ -0,0 +1,14 @@
+package br.com.springboot.restspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Setup {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Setup.class, args);
+	}
+
+}
