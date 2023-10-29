07624600812fdaf052f08d0ce35703bde99b2c71
Santos <tuany.a.santos@accenture.com>
2023-10-29 16:34:05 -0300

    add repository on services

--- project-01/rest-spring-boot/src/main/java/br/com/springboot/restspringboot/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ project-01/rest-spring-boot/src/main/java/br/com/springboot/restspringboot/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.com.springboot.restspringboot.exception.handler;
 
 import br.com.springboot.restspringboot.exception.ExceptionResponse;
-import br.com.springboot.restspringboot.exception.UnsupportedMathOperationException;
+import br.com.springboot.restspringboot.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,15 +26,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         );
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handlerBadRequest (
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions (
             Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false)
         );
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
 
